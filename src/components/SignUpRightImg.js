df4f91aa4935f4e5241086961ddf5ef7dcbbd400
anoopraju31 <59496739+anoopraju31@users.noreply.github.com>
2022-08-08 20:01:34 +0530

    Add files via upload

---
+++ src/components/SignUpRightImg.js
@@ -0,0 +1,11 @@
+import main from '../img/main3.svg'
+
+const SignupRightImg = () => {
+  return (
+    <div className="w-[100%] h-screen sm:hidden md:hidden lg:flex justify-center items-centerms- bg-[#F4F4F4]">
+        <img className='w-[80%] h-auto' src={main} alt='' />
+    </div>
+  )
+}
+
+export default SignupRightImg
